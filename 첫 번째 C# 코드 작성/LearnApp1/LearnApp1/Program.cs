634786315272d921be4c515e8da06ea2a7ac1f37
chatterbox <ibaedori@outlook.com>
2022-07-01 21:46:15 +0900

    learn #1

---
+++ 첫 번째 C# 코드 작성/LearnApp1/LearnApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*1.
+Hello World!
+*/
+Console.WriteLine("Hello, World!");
+
+// 2. 'console' 이름이 현재 컨텍스트에 없습니다.
+//console.WriteLine("Hello World!");
+
+// 3. 문자 리터럴에 문자가 너무 많습니다.
+//Console.WriteLine('Hello World!');
+
+/*4. 
+Congratulations! You wrote your first lines of code!
+*/
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code!");
+
+/*5.
+This is the first line.
+This is the second line.
+*/
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
